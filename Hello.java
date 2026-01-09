a18d9d01f112115aa3e6d92907756d1ee31e84fe
Artem <karp.artemiy@gmail.com>
2026-01-09 23:41:36 +0200

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -1,13 +1,13 @@
-public class Hello { 
+public class Hello 
+{ 
     public static void main( String argv[] ) 
     { 
+        // Default is "World" 
+        // Author: Jim Weirich (jim@somewhere.com)  
         String name = "World"; 
-        if ( argv.length != 0 ) 
-        { 
+        if ( argv.length != 0 ) { 
             name = argv[0]; 
         } 
-        System.out.println( "Hello, " + name + "!" ); 
+        System.out.println( "Hello, " + name + "!"); 
     } 
-} 
-
-// Default is "World"
+}  
