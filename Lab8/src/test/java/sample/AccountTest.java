c94ca52c7f9fb34f0e644fb9ddd9e745d992a2e9
Artem <karp.artemiy@gmail.com>
2026-01-11 18:25:15 +0200

    Lab 8: Initial code with Account and Customer (step 2.1)

---
+++ Lab8/src/test/java/sample/AccountTest.java
@@ -0,0 +1,58 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void bankcharge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void overdraftFee() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDaysOverdrawn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIban() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIban() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMoney() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMoney() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrency() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrency() {
+    }
+}
---
+++ Lab8/src/test/java/sample/CustomerTest.java
@@ -0,0 +1,46 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCustomerType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCustomerType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCustomerDaysOverdrawn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCustomerMoney() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCustomerAccount() {
+    }
+}
